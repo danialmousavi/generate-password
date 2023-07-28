4c30828f96afac34b1011b02b904ba42a096a3fa
Danial <danialmoosavi69@gmail.com>
2023-07-28 22:17:59 +0330

    generate password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CGBS';
+  password: String='';
+  includelatter: boolean = false;
+  includesymbol: boolean = false;
+  includenumbers: boolean = false;
+  length: number = 0;
+
+  onButtonClick() {
+    const numbers='0123456789';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols='!@#$%$^%^%*&()';
+    let validchar='';
+    if(this.includelatter){
+      validchar+=letters;
+    }
+    if(this.includenumbers){
+      validchar+=numbers;
+    }
+    if(this.includesymbol){
+      validchar+=symbols;
+    }
+    let generatedpassword='';
+    for(var index=0;index<this.length;index++){
+      let index=Math.floor(Math.random()*validchar.length);
+      generatedpassword+=validchar[index];
+    }
+    this.password = generatedpassword;
+
+  }
+  getpassword() {
+    return this.password;
+  }
+  onlatterchange() {
+    this.includelatter = !this.includelatter;
+  }
+  onnumberschange() {
+    this.includenumbers = !this.includenumbers;
+  }
+  onsymbolschange() {
+    this.includesymbol = !this.includesymbol;
+  }
+  onChangeLength(target: any) {
+    const parsvalue = parseInt(target.value);
+    if (!isNaN(parsvalue)) {
+      this.length = parsvalue;
+    }
+  }
 }
